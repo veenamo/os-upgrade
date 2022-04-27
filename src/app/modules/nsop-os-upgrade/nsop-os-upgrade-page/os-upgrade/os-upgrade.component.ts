1be0c896ee7a03f5126aec020ade65e048cd2489
veena <veena9665@gmail.com>
2022-04-27 20:38:00 +0530

    "adding the table"

--- src/app/modules/nsop-os-upgrade/nsop-os-upgrade-page/os-upgrade/os-upgrade.component.ts
+++ src/app/modules/nsop-os-upgrade/nsop-os-upgrade-page/os-upgrade/os-upgrade.component.ts
@@ -3,13 +3,87 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-os-upgrade',
   templateUrl: './os-upgrade.component.html',
-  styleUrls: ['./os-upgrade.component.css']
+  styleUrls: ['./os-upgrade.component.css'],
 })
 export class OsUpgradeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  toggle = true;
+  dataSource: object[] = [];
+  displayedColumns: string[] = [
+    'osVersion',
+    'platformName',
+    'operations',
+    'details',
+  ];
+  dataToDisplay = [
+    {
+      osVersion: 'v 1',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'H',
+    },
+    {
+      osVersion: 'v 2',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'Hecc',
+    },
+    {
+      osVersion: 'v 3',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'Licc',
+    },
+    {
+      osVersion: 'v 4',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'Be',
+    },
+    {
+      osVersion: 'v 5',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'B',
+    },
+    {
+      osVersion: 'v 6',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'C',
+    },
+    {
+      osVersion: 'v 7',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'N',
+    },
+    {
+      osVersion: 'v 8',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'O',
+    },
+    {
+      osVersion: 'v 9',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'F',
+    },
+    {
+      osVersion: 'v 10',
+      platformName: 'MX400 router',
+      operations: 'edit',
+      details: 'Ne',
+    },
+  ];
 
-  ngOnInit(): void {
+  addData() {
+    this.toggle = !this.toggle;
+    //this.status = this.toggle ? 'Enable' : 'Disable';
   }
-
+  ngOnInit(): void {
+    this.dataSource = this.dataToDisplay;
+  }
 }
--- src/app/modules/nsop-os-upgrade/nsop-os-upgrade.module.ts
+++ src/app/modules/nsop-os-upgrade/nsop-os-upgrade.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
 
 import { OsUpgradeComponent } from './nsop-os-upgrade-page/os-upgrade/os-upgrade.component';
 import { OsUpgradeRoutingModule } from './nsop-os-upgrade-routing.module';
@@ -9,6 +10,6 @@ import { HomeComponent } from './nsop-os-upgrade-page/home/home.component';
 @NgModule({
   declarations: [OsUpgradeComponent, HomeComponent],
   exports: [OsUpgradeComponent],
-  imports: [RouterModule, CommonModule, OsUpgradeRoutingModule],
+  imports: [RouterModule, CommonModule, MatTableModule, OsUpgradeRoutingModule],
 })
 export class OsupgradeModule {}
