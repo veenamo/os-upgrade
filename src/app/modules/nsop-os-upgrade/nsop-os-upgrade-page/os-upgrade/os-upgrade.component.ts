f96320a87d03727daf9942c779320854c5e50c3d
veena <veena9665@gmail.com>
2022-05-02 11:31:38 +0530

    "added the upgrade popup operations(remove)"

--- src/app/modules/nsop-os-upgrade/nsop-os-upgrade-page/os-upgrade/os-upgrade.component.ts
+++ src/app/modules/nsop-os-upgrade/nsop-os-upgrade-page/os-upgrade/os-upgrade.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTable } from '@angular/material/table';
+
+export interface PeriodicElement {
+  platform: string;
+  osVersion: string;
+  endOfLife: string;
+  endOfSupport: string;
+  endOfSale: string;
+  minCpu: string;
+  minRam: string;
+  minDiskSpace: string;
+}
 
 @Component({
   selector: 'app-os-upgrade',
@@ -6,16 +21,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./os-upgrade.component.css'],
 })
 export class OsUpgradeComponent implements OnInit {
-  constructor() {}
+  @ViewChild('mymodal') maintable: MatTable<PeriodicElement>;
+  @ViewChild('mymodalm') childtable: MatTable<PeriodicElement>;
 
+  constructor(private modalService: NgbModal) {}
+
+  closeResult: string = '';
   toggle = true;
   dataSource: object[] = [];
+  dataPopUpAdd: object[] = [];
+  displayedPopUpColumns: string[] = [
+    'popUposVersion',
+    'popUpEndofLife',
+    'popUpEndofSupport',
+    'popUpEndofSale',
+    'popUpMinCpu',
+    'popUpMinRam',
+    'popUpMinSpace',
+    'popUpoperations',
+  ];
   displayedColumns: string[] = [
     'osVersion',
     'platformName',
     'operations',
     'details',
   ];
+
+  states: string[] = ['a', 'b', 'c'];
+
+  // dataToDisplay = [];
   dataToDisplay = [
     {
       osVersion: 'v 1',
@@ -79,9 +113,74 @@ export class OsUpgradeComponent implements OnInit {
     },
   ];
 
-  addData() {
-    this.toggle = !this.toggle;
-    //this.status = this.toggle ? 'Enable' : 'Disable';
+  form = new FormGroup({
+    platform: new FormControl('', Validators.required),
+    osVersion: new FormControl(''),
+    endOfLife: new FormControl(),
+    endOfSupport: new FormControl(),
+    endOfSale: new FormControl(),
+    minCpu: new FormControl('', Validators.required),
+    minRam: new FormControl('', Validators.required),
+    minDiskSpace: new FormControl('', Validators.required),
+  });
+
+  @ViewChild('mymodalm', { static: false, read: ElementRef }) inRef;
+
+  addOsTemporarily() {
+    console.log('vvvvvvvv', this.form);
+
+    this.dataPopUpAdd = [this.form.value, ...this.dataPopUpAdd];
+  }
+
+  addData(content: any) {
+    this.toggle = false;
+
+    this.modalService
+      .open(content, {
+        ariaLabelledBy: 'modal-basic-title',
+        windowClass: 'modal-claass',
+        size: 'lg',
+      })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      this.toggle = true;
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      this.toggle = true;
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  popUpRemove(action: string, index: number, mymodalm) {
+    let choice = confirm('Are you sure you want to delete?');
+    console.log('nnnnnnnn', mymodalm);
+    if (index > -1) {
+      this.dataPopUpAdd.splice(index, 1);
+      mymodalm.renderRows();
+    }
+  }
+
+  popUpEdit(action: string) {
+    console.log(console.log('nnnnnnnn', action));
+  }
+
+  submit() {
+    console.log(this.dataPopUpAdd);
+    this.dataSource = [...this.dataPopUpAdd, ...this.dataSource];
+    this.modalService.dismissAll('Cross click');
+    this.dataPopUpAdd = [];
   }
   ngOnInit(): void {
     this.dataSource = this.dataToDisplay;
--- src/app/modules/nsop-os-upgrade/nsop-os-upgrade.module.ts
+++ src/app/modules/nsop-os-upgrade/nsop-os-upgrade.module.ts
@@ -2,14 +2,37 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
-
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
 import { OsUpgradeComponent } from './nsop-os-upgrade-page/os-upgrade/os-upgrade.component';
 import { OsUpgradeRoutingModule } from './nsop-os-upgrade-routing.module';
 import { HomeComponent } from './nsop-os-upgrade-page/home/home.component';
+import { AddProjectDetailsComponent } from './nsop-os-upgrade-page/add-project-details/add-project-details.component';
 
 @NgModule({
-  declarations: [OsUpgradeComponent, HomeComponent],
+  declarations: [OsUpgradeComponent, HomeComponent, AddProjectDetailsComponent],
   exports: [OsUpgradeComponent],
-  imports: [RouterModule, CommonModule, MatTableModule, OsUpgradeRoutingModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    OsUpgradeRoutingModule,
+  ],
+  providers: [MatDatepickerModule],
 })
 export class OsupgradeModule {}
